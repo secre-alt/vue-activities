cd10a1cf12e7ecb904cf773b11ba7566922fbe94
secre-alt <triannesano@gmail.com>
2025-08-30 21:44:35 +0800

    Add Activity 1 project

---
+++ vue-activities/activity-1/Task/src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue'
+
+
+const message = ref('Task');
+const newTask = ref("");
+
+function formSubmitted() {
+  console.log(newTask.value);
+}
+</script>
+
+<template>
+   <main>
+     <h1>{{ message }}</h1>
+     <form>
+        <label>
+          New Task
+          <input v-model="newTask" name="newTask"/>
+        </label>
+        <div class=" button-container">
+          <button>Add</button>
+        </div>
+        <!-- <h3>{{ newTask }}</h3> -->
+      </form>
+
+    </main>
+  </template>
+
+
+  <style scoped>
+  main {
+    max-width: 800px;
+    margin: 1rem auto;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .button-container {
+    display: flex;
+    justify-content: end;
+    
+  }
+  </style>
