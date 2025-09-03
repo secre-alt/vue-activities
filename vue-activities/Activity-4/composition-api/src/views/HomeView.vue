aa9616015b1d2e82c251c3bfb9aa2178c79d962a
secre-alt <triannesano@gmail.com>
2025-09-03 13:34:41 +0800

    up

--- vue-activities/Activity-4/composition-api/src/views/HomeView.vue
+++ vue-activities/Activity-4/composition-api/src/views/HomeView.vue
@@ -608,14 +608,16 @@ const posts = [
  <div v-for="post in posts" :key="post.id">
    <div class="wrapper">
     <div class="header">
-      <span>Written by NAME on DATE</span>
+      <span>POST ID NO. {{ post.id }}</span>
       <div>
         <button class="del material-icons">delete</button>
           <button class="save material-icons">bookmark_border</button>
       </div>
     </div>
-    <h1>{{ post.title }}</h1>
-    <p>{{ post.body}}</p>
+    <div class="content">
+      <h1>{{ post.title }}</h1>
+      <p>{{ post.body}}</p>
+    </div>
   </div>
  </div>
 </template>
@@ -642,32 +644,43 @@ const posts = [
     button {
       border-radius: 30px;
     }
-     .del {
-      font-size: 20px;
-      padding: 3px;
-      background: #fff;
-      color: #ef4444;   
+
+    div {
+      display: flex;
+      gap: 0.5rem;
+      align-items: center;
+
+       .del {
+          font-size: 20px;
+          padding: 3px;
+          background: #fff;
+          color: #ef4444;   
+        }
+        .save {
+          font-size: 20px;
+          padding: 3px;
+          background: #fff;
+          color: #14a841;
+      }
     }
-     .save {
-      font-size: 20px;
-      padding: 3px;
-      background: #fff;
-      color: #14a841;
-  }
 }
-    h1 {
-      font-size: 1.25rem;
-      font-weight: 600;
-      margin-bottom: 0.75rem;
+   .content {
+    padding: 1rem;
+    
+     h1 {
+      font-size: 1.5rem;
+      font-weight: 700;
+      margin-bottom: 0.5rem;
       color: #1e293b;
       text-transform: capitalize;
     }
 
     p {
-      font-size: 0.95rem;
-      line-height: 1.6;
+      font-size: 1rem;
+      line-height: 1.5;
       color: #475569;
       white-space: pre-line;
     }
+   }
 }
 </style>
