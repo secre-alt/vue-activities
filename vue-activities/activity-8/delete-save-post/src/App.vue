3ca62468002f1633132048a2f2d2f717ca358acb
secre-alt <triannesano@gmail.com>
2025-09-20 02:37:24 +0800

    added activity-8

---
+++ vue-activities/activity-8/delete-save-post/src/App.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink to="/" class="nav-link">
+            <span class="material-icons">home</span>
+            Home</RouterLink>
+        <RouterLink to="/post-create" class="nav-link">
+           <span class="material-icons">add_circle</span>
+            New Post</RouterLink>
+      </nav>
+  </header>
+  <RouterView />
+</template>
+
+<style lang="scss" scoped>
+  nav {
+    padding: 1rem 2rem;
+    background: #1571f0;
+    border-bottom: 1px solid #60a5fa;
+
+    .nav-link {
+      display: inline-flex;
+      align-items: center;
+      gap: 0.5rem;
+      color: #fff;
+      padding: 5px 15px;
+      border-radius: 5px;
+      transition: all 0.2s ease-in-out;
+      margin-right: 1rem;
+      position: relative;
+      text-decoration: none;
+
+      .material-icons {
+        font-size: 20px;
+      }
+      
+      &::after {
+      content: '';
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 0;
+      height: 2px;
+      background: #6eafff;
+      transition: width 0.3s ease-in-out;
+    }
+
+    &:hover::after {
+      width: 100%;
+    }
+
+    &.router-link-exact-active::after {
+      width: 100%;   
+    }
+    }
+  }
+</style>
---
+++ vue-activities/activity-8/delete-save-post/src/components/PostItem.vue
@@ -0,0 +1,84 @@
+<script setup>
+import { usePostsStore } from '@/stores/posts';
+
+const postStore = usePostsStore()
+
+defineProps({
+post: {
+    type: Object,
+    required: true
+}
+})
+
+</script>
+
+<template>
+    <div>
+     <div class="header">
+        <span>POST ID NO. {{ post.id }}</span>
+        <div>
+            <button @click.stop="postStore.deletePost(post.id)" class="del material-icons">delete</button>
+            <button @click.stop="postStore.savePost(post.id)" class="save material-icons">{{ post.is_saved? 'bookmark': 'bookmark_border' }}</button>
+        </div>
+        </div>
+        <div class="content">
+        <h1>{{ post.title }}</h1>
+        <p>{{ post.body}}</p>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+  .header {
+    font-size: 20px;
+    font-style: bold;
+    background: #1571f0;
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #fff;
+    font-weight: 300;
+    button {
+      border-radius: 30px;
+    }
+
+    div {
+      display: flex;
+      gap: 0.5rem;
+      align-items: center;
+
+       .del {
+          font-size: 20px;
+          padding: 3px;
+          background: #fff;
+          color: #ef4444;   
+        }
+        .save {
+          font-size: 20px;
+          padding: 3px;
+          background: #fff;
+          color: #14a841;
+      }
+    }
+}
+   .content {
+    padding: 1rem;
+    
+     h1 {
+      font-size: 1.5rem;
+      font-weight: 700;
+      margin-bottom: 0.5rem;
+      color: #1e293b;
+      text-transform: capitalize;
+    }
+
+    p {
+      font-size: 1rem;
+      line-height: 1.5;
+      color: #475569;
+      white-space: pre-line;
+    }
+}
+</style>
